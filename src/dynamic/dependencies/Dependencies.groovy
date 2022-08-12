c572c9caf1b4014b151961d01c48309310af5a18
Felipe Barbosa <lybrbarbosa@gmail.com>
2022-08-12 11:18:41 -0300

    build: rollback swap in docker image

--- src/dynamic/dependencies/Dependencies.groovy
+++ src/dynamic/dependencies/Dependencies.groovy
@@ -9,7 +9,7 @@ class Dependencies{
                     image: 'node:18-alpine', 
                     ttyEnabled: true, 
                     command: '/bin/sh',
-                    resourceRequestMemory: '1024'
+                    resourceRequestMemory: '2046'
                 )
             ],
             yamlMergeStrategy: jenkins.merge(),
@@ -22,15 +22,6 @@ class Dependencies{
         {
             jenkins.node(jenkins.POD_LABEL){
                 jenkins.container('node'){
-                    jenkins.sh label: "Prepare for install",
-                        returnStdout: true, 
-                        script: """
-                        sudo fallocate -l 1G /swapfile
-                        sudo chmod 600 /swapfile
-                        sudo mkswap /swapfile
-                        sudo swapon /swapfile
-                        echo "swapfile  none  swap  sw  0   0" >> /etc/fstab
-                    """
                     jenkins.sh label: "Installing dependencies", script: "npm install"
                     def packageJson = jenkins.readJSON file: 'package.json'
                     jenkins.env.APP_VERSION = packageJson.version
