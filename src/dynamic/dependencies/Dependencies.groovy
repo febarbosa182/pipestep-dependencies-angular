22a00bb407aead6629c43b7eee8710b96d49063b
Felipe Barbosa <lybrbarbosa@gmail.com>
2022-08-15 11:13:47 -0300

    build: npm ci cmd

--- src/dynamic/dependencies/Dependencies.groovy
+++ src/dynamic/dependencies/Dependencies.groovy
@@ -6,7 +6,7 @@ class Dependencies{
             containers: [
                 jenkins.containerTemplate(
                     name: 'node', 
-                    image: 'node:18-alpine', 
+                    image: 'node:18-bullseye-slim', 
                     ttyEnabled: true, 
                     command: '/bin/sh',
                     resourceRequestMemory: '2046'
