29693a62b10b33f75e2ba382bb9ab1c28ba52218
Felipe Barbosa <lybrbarbosa@gmail.com>
2022-09-01 19:31:46 -0300

    build: set swap

--- src/dynamic/dependencies/Dependencies.groovy
+++ src/dynamic/dependencies/Dependencies.groovy
@@ -29,12 +29,13 @@ class Dependencies{
             jenkins.node(jenkins.POD_LABEL){
                 jenkins.container('node'){
                     jenkins.sh label: "Prepare environment", script: """
+                      set +e
                       apt update && apt upgrade -y
                       apt install sudo
                       sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
                       sudo /sbin/mkswap /var/swap.1
-                      sudo /sbin/swapon /var/swap.1 || exit 0
-                      """, returnStdout: true, returnStatus: false
+                      sudo /sbin/swapon /var/swap.1
+                      """, returnStatus: false
                     jenkins.sh label: "Installing dependencies", script: "npm install --unsafe-perm=true --allow-root"
                     def packageJson = jenkins.readJSON file: 'package.json'
                     jenkins.env.APP_VERSION = packageJson.version
