f746f07c301b741b98a012358bb81d0d0ad27030
Felipe Barbosa <lybrbarbosa@gmail.com>
2022-09-01 20:30:48 -0300

    build: set swap

--- src/dynamic/dependencies/Dependencies.groovy
+++ src/dynamic/dependencies/Dependencies.groovy
@@ -34,7 +34,7 @@ class Dependencies{
                       apt install sudo
                       sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
                       sudo /sbin/mkswap /var/swap.1
-                      sudo chmod 600 /var/swap.1
+                      chown root /var/swap.1 && sudo chmod 600 /var/swap.1
                       sudo /sbin/swapon /var/swap.1
                       """
                     jenkins.sh label: "Installing dependencies", script: "npm install"
